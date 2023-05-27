aa201330029a417939316c60686a70086fd6a40f
AmbujShukla6456 <shukla.ambuj.2016626@gmail.com>
2023-05-28 00:14:29 +0530

    first commit

---
+++ funding/src/App.js
@@ -0,0 +1,135 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "./utils/load-contract";
+
+function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+    contract: null,
+    addr:null,
+  });
+
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [reload, shouldReload] = useState(false);
+
+  const reloadEffect = () => shouldReload(!reload);
+  const setAccountListener = (provider) => {
+    provider.on("accountsChanged", (accounts) => setAccount(accounts[0]));
+  };
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+      const contract = await loadContract("Funder", provider);
+      const addr=contract.address
+      if (provider) {
+        setAccountListener(provider);
+        provider.request({ method: "eth_requestAccounts" });
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+          contract,
+          addr,
+        });
+      } else {
+        console.error("Please install MetaMask!");
+      }
+      // if (window.ethereum) {
+      //   provider = window.ethereum;
+      //   try {
+      //     await provider.enable();
+      //   } catch {
+      //     console.error("User is not allowed");
+      //   }
+      // } else if (window.web3) {
+      //   provider = window.web3.currentProvider;
+      // } else if (!process.env.production) {
+      //   provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      // }
+    };
+
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api;
+      const balance = await web3.eth.getBalance(contract.address);
+      setBalance(web3.utils.fromWei(balance, "ether"));
+    };
+    web3Api.contract && loadBalance();
+  }, [web3Api, reload]);
+
+  const transferFund = async () => {
+    const { web3, contract } = web3Api;
+    await contract.transfer1({
+      from: account,
+      value: web3.utils.toWei("2", "ether"),
+    });
+    reloadEffect();
+  };
+
+  const withdrawFund = async () => {
+    const { provider,addr,contract, web3 } = web3Api;
+    const withdrawAmount = web3.utils.toWei("2", "ether");
+    const accounts=await provider.request({
+      method: "eth_requestAccounts",
+    });
+    await contract.withdraw(withdrawAmount,{
+      from: addr,
+      to:accounts[0],
+      value:withdrawAmount
+    });
+    reloadEffect();
+  };
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
+  // console.log(web3Api.web3);
+  return (
+    <>
+      <div className="card text-center">
+        <div className="card-header">Funding</div>
+        <div className="card-body">
+          <h5 className="card-title">Balance: {balance} ETH </h5>
+          <p className="card-text">
+            Account : {account ? account : "not connected"}
+          </p>
+          {/* <button
+            type="button"
+            className="btn btn-success"
+            onClick={async () => {
+              const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+              });
+              console.log(accounts);
+            }}
+          >
+            Connect to metamask
+          </button> */}
+          &nbsp;
+          <button type="button" className="btn btn-success " onClick={transferFund}>
+            Transfer
+          </button>
+          &nbsp;
+          <button type="button" className="btn btn-primary " onClick={withdrawFund}>
+            Withdraw
+          </button>
+        </div>
+        <div className="card-footer text-muted">Ambuj SHukla</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+//npm install --save react-scripts@4.0.3
